d11e29382a4f8a2bacc4a06af8380465adfbfb78
Subbu Tech Tutorials <subbutechtutorials@gmail.com>
2024-10-21 01:13:17 +0530

    Update run-smoke-tests.sh

--- scripts/run-smoke-tests.sh
+++ scripts/run-smoke-tests.sh
@@ -2,7 +2,7 @@
 
 echo "Running Smoke Tests..."
 
-# Use the public DNS or IP of your EC2 instance
+# Use the public DNS or IP of your load balancer
 PUBLIC_IP_OR_DOMAIN="abeef2b5efecf47f48fd7141e3079c27-359075947.ap-south-1.elb.amazonaws.com"
 APP_PORT=8081
 
@@ -12,12 +12,13 @@ waitTime=10
 
 for i in $(seq 1 $maxRetries); do
     response=$(curl -s -o /dev/null -w "%{http_code}" http://${PUBLIC_IP_OR_DOMAIN}:${APP_PORT}/actuator/health)
-    
+    echo "Attempt $i/$maxRetries: Response code: $response"
+
     if [ "$response" -eq 200 ]; then
         echo "Smoke Test Passed: Application is healthy."
         exit 0
     else
-        echo "Attempt $i/$maxRetries: Application is not healthy yet, retrying in $waitTime seconds..."
+        echo "Application is not healthy yet, retrying in $waitTime seconds..."
         sleep $waitTime
     fi
 done
