ef79c2d8f6ecd29436186675c4e61fd2d8ba7454
Subbu Tech Tutorials <subbutechtutorials@gmail.com>
2024-10-20 22:03:17 +0530

    Update run-smoke-tests.sh

--- scripts/run-smoke-tests.sh
+++ scripts/run-smoke-tests.sh
@@ -2,7 +2,7 @@
 
 echo "Running Smoke Tests..."
 
-# Check if the application is running (actuator health check)
+# Check if the application is running on the correct port (8081 in this case)
 response=$(curl -s -o /dev/null -w "%{http_code}" http://localhost:8081/actuator/health)
 
 if [ "$response" -eq 200 ]; then
